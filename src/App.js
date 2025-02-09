1cd8aa2d74e5dea38fcf5635f6d6db2fcf20c9a9
abdulmoizz4423 <abdulmoiz@codetokloud.com>
2025-02-09 09:34:01 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,46 +4,45 @@ import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import Homeintro from './components/Homeintro/Homeintro';
-import Fucntional from './components/Functional/Fucntional';
+
+// Introduced a typo in the import statement
+import Functional from './components/Functional/Fucntional'; 
 import Payment from './components/Payment/Payment';
 import Purchaseinfo from './components/Purchaseinfo/Purchaseinfo';
 import Footer from './components/Footer/Footer';
 
-
 function App() {
-
   const [topbanner, setTopBanner] = useState('solo');
   const [functonalTile, setFunctonalTile] = useState('ranked');
   const [value, setValue] = useState([0, 1000]);
   const [pref, setPref] = useState(1);
-
   const [totalPrice, setTotalPrice] = useState(0);
-
   const [funcAddon1, setfuncAddon1] = useState([]);
 
-  // console.log(funcAddon1)
+  // Introduced an environment variable dependency that will cause the app to fail if not set
+  if (!process.env.REACT_APP_API_URL) {
+    throw new Error("Missing REACT_APP_API_URL. Application cannot start.");
+  }
+
   return (
     <div className="App">
       <Header />
-      <Homeintro topbanner= {topbanner}/>
-      <Fucntional 
-        topbanner= {topbanner} 
-        setTopBanner = {setTopBanner}
+      <Homeintro topbanner={topbanner} />
+      <Functional 
+        topbanner={topbanner} 
+        setTopBanner={setTopBanner}
         functonalTile={functonalTile}
         setFunctonalTile={setFunctonalTile}
-        pref = {pref}
+        pref={pref}
         setPref={setPref}
-        value ={value}
+        value={value}
         setValue={setValue}
-
         totalPrice={totalPrice}
         setTotalPrice={setTotalPrice}
-
-
         funcAddon1={funcAddon1}
         setfuncAddon1={setfuncAddon1}
-        />
-      <Payment  value={value} />
+      />
+      <Payment value={value} />
       <Purchaseinfo />
       <Footer /> 
     </div>
